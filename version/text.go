d681223ed926427f3fc1ee7f21144ee9d854e00c
Helios <i@shangyes.net>
2019-12-13 17:22:23 +0800

    v0.1.0-0.0.5-Version
     [ADD] Check Latest Version
    
     close #10

--- version/text.go
+++ version/text.go
@@ -1,13 +1,21 @@
-package text
+package version
 
 var (
-	VersionDetail = `当前版本进展:
+	tipQuery         = "获取最新版本信息中"
+	tipAlreadyLatest = "当前已是最新版本"
+	tipLatest        = "最新版本: %s\n"
+
+	errNet = "请检查网络连接"
+
+	failQuery = "获取失败"
+
+	detail = `当前版本进展:
   Login
     [=]  基础登陆
-    [ ]  保存账号
+    [=]  保存账号
     [=]  Cookie登陆
     [=]  伪装设备
-    [ ]  状态持久化
+    [=]  状态持久化
   
   Logout
     [ ]  基础登出
@@ -28,12 +36,12 @@ var (
     [ ]  指定设备下线
 
   Test
-    [ ]  校园网连通性测试
+    [=]  校园网连通性测试
     [ ]  校园网测速
 
   Version
-    [ ] 检查版本
-    [ ] 自动更新
+    [ ]  检查版本
+    [ ]  自动更新
     [=]  有功能想法请发送邮件至 i@shangyes.net
 `
 )
--- version/version.go
+++ version/version.go
@@ -2,20 +2,74 @@ package version
 
 import (
 	"fmt"
+	"io/ioutil"
 	"ipgw/base"
-	"ipgw/text"
+	"ipgw/base/cfg"
+	"ipgw/base/ctx"
+	"os"
+	"regexp"
 )
 
 var CmdVersion = &base.Command{
 	UsageLine: "ipgw version",
 	Short:     "版本查询",
-	Long:      "输出ipgw的版本信息, 包括当前版本与草案功能",
+	Long: `输出ipgw的版本信息
+  -u    查看最新版本
+  -v    输出完整版本功能
+
+  ipgw version
+    查看版本
+  ipgw version -u
+    查看最新版本
+  ipgw version -v
+    查看当前版本完整功能
+`,
 }
 
+var u bool
+
 func init() {
+	CmdVersion.Flag.BoolVar(&u, "u", false, "")
+	CmdVersion.Flag.BoolVar(&cfg.FullView, "v", false, "")
+
 	CmdVersion.Run = runVersion // break init cycle
 }
 
 func runVersion(cmd *base.Command, args []string) {
-	fmt.Printf("%s\n%s", base.IPGW.Long, text.VersionDetail)
+	fmt.Println(base.IPGW.Long)
+
+	if cfg.FullView {
+		fmt.Print(detail)
+	}
+
+	if u {
+		client := ctx.GetClient()
+		fmt.Println(tipQuery)
+
+		resp, err := client.Get("https://api.github.com/repos/imyown/ipgw/releases/latest")
+		if err != nil {
+			fmt.Fprint(os.Stderr, errNet)
+			os.Exit(2)
+		}
+
+		res, err := ioutil.ReadAll(resp.Body)
+		_ = resp.Body.Close()
+		body := string(res)
+
+		tagExp := regexp.MustCompile(`"tag_name":"(.+?)"`)
+		tags := tagExp.FindAllStringSubmatch(body, -1)
+
+		if len(tags) == 0 {
+			fmt.Println(failQuery)
+			return
+		}
+
+		tag := tags[0][1]
+		if tag == cfg.Version {
+			fmt.Println(tipAlreadyLatest)
+			return
+		}
+		fmt.Printf(tipLatest, tag)
+	}
+
 }
