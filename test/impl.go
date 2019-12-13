81e80e5261d2db491fb052ad38c0164a3eba89d1
Helios <i@shangyes.net>
2019-12-13 16:48:40 +0800

    v0.1.0-0.0.3-Test
     [ADD] Basic Test
     [MOD] Test Text
    
     close #6

--- test/impl.go
+++ test/impl.go
@@ -4,42 +4,60 @@ import (
 	"fmt"
 	"ipgw/base/cfg"
 	"ipgw/base/ctx"
-	"ipgw/text"
 	"strings"
+	"time"
 )
 
 func testImpl() {
 	client := ctx.GetClient()
+	client.Timeout = time.Second
 
+	if cfg.FullView {
+		fmt.Println(testNetTip)
+	}
+
+	// 测试是否连接上校园网
 	_, err := client.Get("https://ipgw.neu.edu.cn")
 
-	if cfg.FullView && err != nil {
-		fmt.Printf("ipgw.neu.edu.cn: %v\n", err)
-	}
-
 	if err != nil {
+		if cfg.FullView {
+			fmt.Printf("ipgw.neu.edu.cn: %v\n", err)
+		}
 		if strings.Contains(err.Error(), "no such host") {
-			fmt.Println(text.TestNotConnect)
+			// 没有联网
+			fmt.Println(testNoInternet)
+			return
+		} else if strings.Contains(err.Error(), "Client.Timeout") {
+			// 没有连校园网
+			fmt.Println(testNotConnect)
 			return
 		}
-		fmt.Printf(text.TestUnexpectedErr, err)
+		fmt.Printf(testUnexpectedErr, err)
 		return
 	}
 
+	fmt.Println(testConnected)
+
+	if cfg.FullView {
+		fmt.Println(testLoggedTip)
+	}
+
+	// 测试是否登陆校园网
 	_, err = client.Get("https://baidu.com")
 
-	if cfg.FullView && err != nil {
-		fmt.Printf("baidu.com: %v\n", err)
-	}
-
 	if err != nil {
-		if strings.Contains(err.Error(), "x509") {
-			fmt.Println(text.TestNotLoggedIn)
+		if cfg.FullView {
+			fmt.Printf("baidu.com: %v\n", err)
+		}
+		// todo 还有可能证书错误，再观望观望
+		if strings.Contains(err.Error(), "Client.Timeout") {
+			// 未登陆
+			fmt.Println(testNotLoggedIn)
 			return
 		}
-		fmt.Printf(text.TestUnexpectedErr, err)
+		fmt.Printf(testUnexpectedErr, err)
 		return
 	}
 
-	fmt.Println(text.TestLoggedIn)
+	fmt.Println(testLoggedIn)
 }
---
+++ test/text.go
@@ -0,0 +1,14 @@
+package test
+
+var (
+	testNoInternet  = "网络连接异常"
+	testNotConnect  = "未连接校园网"
+	testNotLoggedIn = "未登陆校园网"
+
+	testUnexpectedErr = "错误: 遇到了意外的错误: %s\n"
+	testLoggedIn      = "已登陆校园网"
+	testConnected     = "已连接校园网"
+
+	testNetTip    = "测试校园网连通性..."
+	testLoggedTip = "测试是否登陆校园网..."
+)
